62989f5dbb5a9a290afccc7dc87f936b8cc15780
Afsar Ali <afsarali273@gmail.com>
2022-08-14 23:08:34 +0800

    adding build spec file

--- reporter/index.js
+++ reporter/index.js
@@ -1,24 +1,24 @@
-var reporter = require('cucumber-html-reporter');
+var reporter = require("cucumber-html-reporter");
 
 var options = {
-    theme: 'bootstrap',
-    brandTitle: 'Final Test Summary',
-    jsonDir: 'reporter/json/',
-    output: 'reporter/cucumber_report.html',
-    screenshotsDirectory: 'reporter/screenshots/',
-    storeScreenshots: true,
-    reportSuiteAsScenarios: true,
-    scenarioTimestamp: true,
-    ignoreBadJsonFile: true,
-    launchReport: false,
-    metadata: {
-        "App Version": "0.3.2",
-        "Test Environment": "STAGING",
-        "Browser": "Chrome  54.0.2840.98",
-        "Platform": "Windows 10",
-        "Parallel": "Scenarios",
-        "Executed": "Remote"
-    }
+  theme: "bootstrap",
+  brandTitle: "Final Test Summary",
+  jsonDir: "reporter/json/",
+  output: "reporter/cucumber_report.html",
+  screenshotsDirectory: "reporter/screenshots/",
+  storeScreenshots: true,
+  reportSuiteAsScenarios: true,
+  scenarioTimestamp: true,
+  ignoreBadJsonFile: true,
+  launchReport: false,
+  metadata: {
+    "App Version": "0.3.2",
+    "Test Environment": "STAGING",
+    Browser: "Chrome  54.0.2840.98",
+    Platform: "Windows 10",
+    Parallel: "Scenarios",
+    Executed: "Remote",
+  },
 };
 
 reporter.generate(options);
